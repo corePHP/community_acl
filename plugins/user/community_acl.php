7811436a34ac752fa31aa4545fa43f6632d94a27
Bernard Robbins <BernardRobbinsIII@gmail.com>
2012-06-19 13:59:46 -0400

    User Deletion
    When a user is deleted, also delete from cACL user table.
    
    Signed-off-by: Bernard Robbins <BernardRobbinsIII@gmail.com>

--- plugins/user/community_acl.php
+++ plugins/user/community_acl.php
@@ -26,14 +26,14 @@ class plgUserCommunity_ACL extends JPlugin
 			$db =& JFactory::getDBO();
 
 			/*$query = "SELECT DISTINCT a.id as role_id, a.group_id, a.redirect_front, a.redirect_admin, b.function_id
-						FROM `#__community_acl_cb_roles` AS a
-							LEFT JOIN `#__community_acl_users` AS b ON b.user_id = a.cb_member_type
-								WHERE `cb_member_type` IN ( ".$member_type." ) ";*/
+			 FROM `#__community_acl_cb_roles` AS a
+			LEFT JOIN `#__community_acl_users` AS b ON b.user_id = a.cb_member_type
+			WHERE `cb_member_type` IN ( ".$member_type." ) ";*/
 			$query = "
-			SELECT role_id, group_id, function_id
-			FROM `#__community_acl_users`
-			WHERE `cb_member_type`
-				IN ( ".$member_type." ) ";
+					SELECT role_id, group_id, function_id
+					FROM `#__community_acl_users`
+					WHERE `cb_member_type`
+					IN ( ".$member_type." ) ";
 
 			$db->setQuery($query);
 			$user_info = $db->loadObjectList();
@@ -41,22 +41,26 @@ class plgUserCommunity_ACL extends JPlugin
 			//$user_info = array_unique($user_info);
 
 			if(count($user_info) > 0 ){
-			for($i=0; $i<count($user_info); $i++){
-				$query = "INSERT INTO `#__community_acl_users` " .
+				for($i=0; $i<count($user_info); $i++){
+					$query = "INSERT INTO `#__community_acl_users` " .
 						 "  SET `user_id` = '" . $user['id'] . "'" .
 						 "	  , `group_id` = '". $user_info[$i]->group_id  . "'" .
 						 "	  , `role_id` = '" . $user_info[$i]->role_id  . "'" .
 						 "	  , `function_id` = '" . $user_info[$i]->function_id  . "'" .
 						 "	  , `redirect_front` = ''" .
 						 "	  , `redirect_admin` = ''"
-						 ;
-				$db->setQuery($query);
-				$db->query();
+						 		;
+						 		$db->setQuery($query);
+						 		$db->query();
 				}
 			}
 		}
 	}
+
+	function onBeforeDeleteUser($user)
+	{
+		$db = JFactory::getDBO();
+		$db->setQuery( "DELETE FROM `#__community_acl_users` WHERE user_id='{$user['id']}'" );
+		$db->query();
+	}
 }
-
-
-?>
